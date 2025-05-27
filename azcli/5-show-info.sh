865bfec1d9ee662d115d55603be5dfbab95fd8cf
Phil Huang <phil.huang@microsoft.com>
2025-05-27 10:45:37 +0800

    🔧 Refactor Azure Firewall private IP variable naming for consistency across scripts
    Signed-off-by: Phil Huang <phil.huang@microsoft.com>

--- azcli/5-show-info.sh
+++ azcli/5-show-info.sh
@@ -32,8 +32,8 @@ echo "ssh $VM_ADMIN_USERNAME@$SPOKE_VM_PUBLIC_IP"
 echo
 
 echo "Firewall 資訊"
-fwprivaddr="$(az network firewall ip-config list -g $RESOURCE_GROUP_NAME -f azfw-hub --query "[?name=='FW-config'].privateIpAddress" --output tsv)"
-echo "Azure Firewall Private IP: $fwprivaddr"
+AZFW_PRIVATE_IP="$(az network firewall ip-config list -g $RESOURCE_GROUP_NAME -f azfw-hub --query "[?name=='FW-config'].privateIpAddress" --output tsv)"
+echo "Azure Firewall Private IP: $AZFW_PRIVATE_IP"
 
 echo
 echo "Password for $VM_ADMIN_USERNAME"
